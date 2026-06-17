cc03c0a38bee4523cea48bb23d3ff03b0548ece1
fury <furlici0uus@gmail.com>
2026-06-17 10:23:12 +0300

    Добавлены автотесты

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,6 +3,12 @@ package base;
 import com.microsoft.playwright.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Paths;
+import java.util.Collections;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class BaseTest {
     Playwright playwright;
@@ -13,15 +19,29 @@ public class BaseTest {
     @BeforeEach
     void setUp() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch();
         context = browser.newContext();
         page = context.newPage();
     }
 
+    @Test
+    void testMockedStatusCode() {
+        page.navigate("https://the-internet.herokuapp.com/status_codes");
+        page.route("**/status_codes/404", route -> {
+            route.fulfill(new Route.FulfillOptions()
+                    .setStatus(200)
+                    .setHeaders(Collections.singletonMap("Content-Type", "text/html"))
+                    .setBody("<h3>Mocked Success Response</h3>")
+            );
+        });
+        page.locator("[href=\"status_codes/404\"]").click();
 
+        assertThat(page.getByText("Mocked Success Response")).isVisible();
+    }
 
     @AfterEach
     void tearDown() {
+        browser.close();
         playwright.close();
     }
 }
