bf5109b6e10bb911a1eb399f8d5b0dcc9ea095f0
fury <furlici0uus@gmail.com>
2026-06-17 11:38:05 +0300

    Практическое задание 3

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,60 +1,59 @@
 package base;
 
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.WaitForSelectorState;
 import org.junit.jupiter.api.*;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Collections;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class BaseTest {
-    static Playwright playwright;
-    static Browser browser;
-    BrowserContext context;
+    private Playwright playwright;
+    private Browser browser;
+    private BrowserContext context;
     public Page page;
-
-    @BeforeAll
-    static void setupAll() {
-        playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-    }
 
     @BeforeEach
     void setup() {
-        context = browser.newContext();
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions().setHeadless(false)
+        );
+        context = browser.newContext(new Browser.NewContextOptions()
+                .setRecordVideoDir(Paths.get("videos/"))
+        );
+
         page = context.newPage();
     }
 
     @Test
-    void simpleInterceptionTest() {
-        page.route("**/authenticate", route -> {
-            System.out.println("Запрос перехвачен!");
-            String postData = route.request().postData();
-            System.out.println("Было: " + postData);
-            String modifiedBody = postData.replace("tomsmith", "HACKED_USER");
-            System.out.println("Стало: " + modifiedBody);
-            route.resume(new Route.ResumeOptions().setPostData(modifiedBody));
-        });
+    void testCartActions() {
+        Locator cart = page.locator("#elements");
 
-        page.navigate("https://the-internet.herokuapp.com/login");
+        page.navigate("https://the-internet.herokuapp.com/add_remove_elements/");
+        page.click("button:text('Add Element')");
+        cart.waitFor(new Locator.WaitForOptions()
+                .setState(WaitForSelectorState.VISIBLE));
+        cart.screenshot(new Locator.ScreenshotOptions()
+                .setPath(getTimestampPath("cart_after_add.png")));
+        page.click("button:text('Delete')");
 
-        page.fill("#username", "tomsmith");
-        page.fill("#password", "SuperSecretPassword!");
-        page.click("button[type='submit']");
-
-        assertThat(page.getByText("Your username is invalid!")).isVisible();
+        page.screenshot(new Page.ScreenshotOptions().setPath(getTimestampPath("cart_after_action.png"))
+        );
     }
 
-    @AfterAll
-    static void tearDownAll() {
-        browser.close();
-        playwright.close();
+    private Path getTimestampPath(String filename) {
+        String timestamp = LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss"));
+
+        return Paths.get("artifacts", timestamp, filename);
     }
 
     @AfterEach
-    void tearDown() {
+    void teardown() {
         context.close();
     }
-
 }
