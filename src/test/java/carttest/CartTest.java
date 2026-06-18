d2a8792b2de45066d5bc466e15708608d677af4c
fury <furlici0uus@gmail.com>
2026-06-18 09:14:44 +0300

    Практическое задание 3

--- src/test/java/carttest/CartTest.java
+++ src/test/java/carttest/CartTest.java
@@ -18,7 +18,7 @@ public class CartTest {
     @BeforeAll
     static void setupAll() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch();
     }
 
     @BeforeEach
