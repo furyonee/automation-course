0ce2f4c5241d99c0f633fdd8bf4781f1c51bd11c
fury <furlici0uus@gmail.com>
2026-07-04 18:48:06 +0300

    Практическое задание 17

--- src/test/java/dynamiccontrolstest/DynamicControlsTest.java
+++ src/test/java/dynamiccontrolstest/DynamicControlsTest.java
@@ -15,7 +15,7 @@ public class DynamicControlsTest {
     void setUp() {
         playwright = Playwright.create();
         browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions().setHeadless(false)
+                new BrowserType.LaunchOptions().setHeadless(tru)
         );
         page = browser.newPage();
     }
